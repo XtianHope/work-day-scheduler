58a7bce383fdbcb54495b984e8595fbc847c6e9b
Christian Tino <xtianhope@gmail.com>
2023-11-19 16:58:12 -0500

    added javascript function to generate time blocks from 9am-5pm, instead of through html...isn't working. May instead implement through html

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,9 @@ $(function () {
   // To display the current date at the top of the page
   $('#currentDay').text(dayjs().format('dddd, MMMM D'));
 
-  // To display the current date
+  // To display the current date & generate time blocks
   displayCurrentDate();
+  generateTimeBlocks();
 
   // To update the displayed date every minute
   setInterval(displayCurrentDate, 60000);
@@ -34,6 +35,28 @@ $(function () {
 
 // To set time block colors on page load
 updateTimeBlocks();
+
+// Function to generate time blocks from 9am-5pm
+function generateTimeBlocks() {
+  var container = $('.container-lg');
+  var businessHours = 9;
+
+  for (var i = 0; i < 8; i++) {
+    var hourId = 'hour-' + businessHours;
+
+    container.append(`
+      <div id="${hourId}" class="row time-block">
+        <div class="col-2 col-md-1 hour text-center py-3">${businessHours > 12 ? businessHours - 12 + 'PM' : businessHours + 'AM'}</div>
+        <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>
+    `);
+
+    businessHours++;
+  }
+}
 
 // To save user input to local storage
 $('.description').each(function () {
