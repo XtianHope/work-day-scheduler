d6e38dda08c2a8bed83a66537b7763d17fc116fb
Christian Tino <xtianhope@gmail.com>
2023-11-19 16:18:53 -0500

    built out time block function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
   // To display the current date at the top of the page
   $('#currentDay').text(dayjs().format('dddd, MMMM D'));
@@ -9,9 +10,20 @@ $(function () {
   function updateTimeBlocks() {
     var currentHour = dayjs().hour();
 
+    $('.time-block').each(function () {
+      const blockHour = parseInt($(this).attr('id').split('-')[1]);
 
+      $(this).removeClass('past present future');
 
-});
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).addClass('present');
+      } else {
+        $(this).addClass('future');
+      }
+    });
+  }
 
 
 
